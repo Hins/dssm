620c3363e0aa94de1b59a7d1b40a3f0eacaf4806
panxiaotong <panxiaotong@xiaomi.com>
2018-07-16 15:22:09 +0800

    fix a bug about doc concatenation

--- single/process_text_data.py
+++ single/process_text_data.py
@@ -58,9 +58,7 @@ if __name__ == "__main__":
     print("calculate bigram count complete")
 
     user_indices = []
-    user_values = []
     doc_indices = []
-    doc_values = []
     for line_index, line in enumerate(input_file):    # <user_query>\001<document1>\t<label1>\002<document2>\t<label2>
         line = line.replace('\n', '').replace('\r', '')
         elements = line.split('\001')
@@ -69,7 +67,6 @@ if __name__ == "__main__":
         user_query_list = elements[0].split(",")
         user_query_len = len(user_query_list)
         query_indice_list = []
-        query_value_list = []
         for index,word in enumerate(user_query_list):
             if index + 1 < user_query_len:
                 key = word + cfg.separator + user_query_list[index + 1]
@@ -77,50 +74,44 @@ if __name__ == "__main__":
                 if key not in bigram_dict:
                     bigram_dict[key] = len(bigram_dict) + 1
                 query_indice_list.append([line_index, bigram_dict[key]])
-                query_value_list.append(1.0)
             else:
                 key = word + cfg.separator + cfg.placeholder
                 #if trigram_count[key] > 5:
                 if key not in bigram_dict:
                     bigram_dict[key] = len(bigram_dict) + 1
                 query_indice_list.append([line_index, bigram_dict[key]])
-                query_value_list.append(1.0)
         if len(query_indice_list) == 0:
             continue
 
         documents = elements[1].split('\002')
         flag = True
         doc_indice_list = []
-        doc_value_list = []
         for document in documents:
             sub_elements = document.split('\t')
             document = sub_elements[0].split(",")
             document_len = len(document)
 
-            prev_size = len(doc_indice_list)
+            tmp_doc_indice_list = []
             for index, word in enumerate(document):
                 if index + 1 < document_len:
                     key = word + cfg.separator + document[index + 1]
                     #if trigram_count[key] > 5:
                     if key not in bigram_dict:
                         bigram_dict[key] = len(bigram_dict) + 1
-                    doc_indice_list.append([line_index, index, bigram_dict[key]])
-                    doc_value_list.append(1.0)
+                    tmp_doc_indice_list.append([line_index, index, bigram_dict[key]])
                 else:
                     key = word + cfg.separator + cfg.placeholder
                     #if trigram_count[key] > 5:
                     if key not in bigram_dict:
                         bigram_dict[key] = len(bigram_dict) + 1
-                    doc_indice_list.append([line_index, index, bigram_dict[key]])
-                    doc_value_list.append(1.0)
-            if prev_size == len(doc_indice_list):
+                    tmp_doc_indice_list.append([line_index, index, bigram_dict[key]])
+            if len(tmp_doc_indice_list) == 0:
                 flag = False
                 break
+            doc_indice_list.append(tmp_doc_indice_list)
         if flag == True:
             user_indices.append(query_indice_list)
-            user_values.append(query_value_list)
             doc_indices.append(doc_indice_list)
-            doc_values.append(doc_value_list)
     input_file.close()
 
     output_file = open(cfg.dict_file_path, 'w')
@@ -140,8 +131,11 @@ if __name__ == "__main__":
     user_indices_output_file.close()
 
     doc_indices_output_file = open(cfg.doc_indices_path, 'w')
-    for item in doc_indices:
-        doc_indices_output_file.write("\001".join(str(sub_item[0]) + "\002" + str(sub_item[1]) for sub_item in item) + "\n")
+    for docs in doc_indices:   # item meant 20 docs
+        doc_list = []
+        for doc in docs:
+            doc_list.append("\002".join(str(indice[0]) + "\003" + str(indice[1]) for indice in doc))
+        doc_indices_output_file.write("\001".join(doc_list) + "\n")
     doc_indices_output_file.close()
 
     sample_size = (line_index + 1) / cfg.batch_size
